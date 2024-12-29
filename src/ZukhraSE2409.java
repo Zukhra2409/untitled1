70247a2449e1f10378e964367cf23adaf91e4944
Zukhra2409 <zukhrakabysheva@gmail.com>
2024-12-29 23:21:49 +0500

    second commit

--- src/ZukhraSE2409.java
+++
@@ -1,7 +0,0 @@
-public class ZukhraSE2409 {
-    private static void ZukhraSE2409(String[] args) {
-        System.out.println("Hello World");
-        Outerclass outer=new Outerclass();
-        outer.useInnerclass();
-    }
-}
